4281f20ad6357bf172d728ad0ab4adcf8a62fbd8
Viktor Kochkin <abbadon389@gmail.com>
2019-03-28 20:32:49 +0700

    Add Users

--- public/index.php
+++ public/index.php
@@ -12,6 +12,19 @@ $configuration = [
 
 $app = new \Slim\App($configuration);
 
+// Контейнеры в этом курсе не рассматриваются (это тема связанная с самим ООП), но если вам интересно, то посмотрите DI Container
+$container = $app->getContainer();
+// Параметром передается базовая директория в которой будут храниться шаблоны
+$container['renderer'] = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря http://php.net/manual/ru/closure.bindto.php
+    return $this->renderer->render($response, 'users/show.phtml', $params);
+});
+
+
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
 });
